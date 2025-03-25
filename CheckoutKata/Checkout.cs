997a07c79aa2f1d66ea6b8946d9eafc71147055a
amaan <amaan.malik@gmail.com>
2025-03-25 09:39:12 +0000

    Implement basic Scan and GetTotal to pass the single item A test

---
+++ CheckoutKata/Checkout.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutKata
+{
+    public class Checkout : ICheckout
+    {
+        private int total = 0;
+
+        public void Scan(string sku)
+        {
+            if (sku == "A")
+                total = 50;
+        }
+
+        public int GetTotalPrice() => total;
+
+    }
+}
