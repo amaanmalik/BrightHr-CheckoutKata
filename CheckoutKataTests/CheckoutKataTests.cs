77274d3f87c980cdb607f37c51ef9aa564844694
amaan <amaan.malik@gmail.com>
2025-03-25 09:50:13 +0000

    Add  parameterized tests for SKU combinations and random order of SKU combinations

--- CheckoutKataTests/CheckoutKataTests.cs
+++ CheckoutKataTests/CheckoutKataTests.cs
@@ -49,5 +49,26 @@ namespace CheckoutKataTests
             Assert.Equal(80, checkout.GetTotalPrice()); // 50 (A) + 30 (B) = 80
         }
 
+        [Theory]
+        [InlineData("AABB", 160)]     // A(50) + A(50) + B(30) + B(30) = 160
+        [InlineData("ABC", 100)]      // A(50) + B(30) + C(20) = 100
+        [InlineData("BCA", 100)]      // A(50) + B(30) + C(20) = 100
+        [InlineData("CAB", 100)]      // A(50) + B(30) + C(20) = 100
+        [InlineData("AAAABBC", 280)] // A(50*4) + B(30*2) + C(20) = 280 
+        public void Scan_MultipleCombinations_ReturnsCorrectTotal(string items, int expectedTotal)
+        {
+            // Arrange
+            ICheckout checkout = new Checkout();
+
+            // Act
+            foreach (char item in items)
+            {
+                checkout.Scan(item.ToString()); 
+            }
+
+            // Assert
+            Assert.Equal(expectedTotal, checkout.GetTotalPrice());
+        }
+
     }
 }
