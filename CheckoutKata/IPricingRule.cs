5a22ed52e2e1377ffb95317e20537324245fa1d8
amaan <amaan.malik@gmail.com>
2025-03-25 10:28:54 +0000

    Add IPricingRule interface for dynamically applying pricing rules to scanned items at checkout. Deouples pricing rules and discounts management from checkout class.

---
+++ CheckoutKata/IPricingRule.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutKata
+{
+    public interface IPricingRule
+    {
+        int Apply(List<string> items);
+    }
+}
