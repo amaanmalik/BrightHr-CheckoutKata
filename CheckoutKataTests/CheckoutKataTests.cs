ac5eb335a02dd03ed7a96384ab7776ec61f3bc89
amaan <amaan.malik@gmail.com>
2025-03-25 09:37:30 +0000

    Add failing test for scanning a single item A with expected total 50

---
+++ CheckoutKataTests/CheckoutKataTests.cs
@@ -0,0 +1,25 @@
+﻿using CheckoutKata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutKataTests
+{
+    public class CheckoutKataTests
+    {
+        [Fact]
+        public void Scan_SingleItemA_Returns50()
+        {
+            // Arrange
+            var checkout = new Checkout();
+
+            // Act
+            checkout.Scan("A");
+
+            // Assert
+            Assert.Equal(50, checkout.GetTotalPrice());
+        }
+    }
+}
