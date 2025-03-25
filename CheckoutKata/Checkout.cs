9b26f809ebd768ac1219ac697e552911f0dbc3cd
amaan <amaan.malik@gmail.com>
2025-03-25 10:11:25 +0000

    Implement multi-buy discount for SKU B (2 for 45)

--- CheckoutKata/Checkout.cs
+++ CheckoutKata/Checkout.cs
@@ -23,14 +23,20 @@ namespace CheckoutKata
             int countA = scannedItems.Count(sku => sku == "A");
             int countB = scannedItems.Count(sku => sku == "B");
             int countC = scannedItems.Count(sku => sku == "C");
+            int countD = scannedItems.Count(sku => sku == "D");
 
             // Apply multi-buy discount for A (3 for 130)
             total += (countA / 3) * 130;               // Apply discount for every set of 3 A's
             total += (countA % 3) * 50;                // Add remaining A's at regular price
 
-            // Regular pricing for B and C
-            total += countB * 30;
+            // Apply multi-buy discount for B (3 for 130)
+            total += (countB / 2) * 45;               // Apply discount for every set of 2 B's
+            total += (countB % 2) * 30;                // Add remaining B's at regular price
+            
+            // Regular pricing for C and D
             total += countC * 20;
+            total += countD * 15;
+
 
             return total;
 
